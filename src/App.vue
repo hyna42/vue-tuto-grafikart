77f64b12785086677ccee25746c98157a6769b9c
hyna42 <bylo@duck.com>
2026-01-24 22:52:08 +0100

    modifier directement une propriete .value.proriete = ???

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,26 @@
 <template>
-  <!-- valeur interpréte d'un attribut -->
-  <h1 :id="`p-${count}`" :style="{ color: count > 5 ? 'red' : 'green' }">
-    Compteur : {{ count }}
-  </h1>
-  <button @click="incrementer">Incrémenter</button>
-  <button @click="decrementer">Décrémenter</button>
-
-  <div v-if="count > 3">
-    <p>Vous avez cliqué plus 3 fois</p>
-  </div>
-
-  <div v-else="count > 3">
-    <p>Vous avez cliqué moins de 3 fois</p>
-  </div>
-
-  <hr />
-  <button @click="sortMovies">Réorganiser</button>
-  <form action="" @submit.prevent="addMovie">
-    <input type="text" placeholder="Nouveau Film" v-model="movieName" />
-    {{ movieName }}
-    <button>Ajouter</button>
-  </form>
-  <!-- afficher liste des films -->
   <ul>
-    <li v-for="movie in movies" :key="movie">
-      {{ movie }}
-      <button @click="deleteMovie(movie)">Supprimer</button>
-    </li>
+    <li>{{ person.firstname }}</li>
+    <li>{{ person.lastname }}</li>
+    <li>{{ person.age }}</li>
   </ul>
+  <button @click.prevent="randomAge">Changer Age</button>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const count = ref(0);
-const incrementer = () => {
-  console.log("count", count);
-  count.value++;
-};
+const person = ref({
+  firstname: "John",
+  lastname: "Doe",
+  age: 20,
+});
 
-const decrementer = (e) => {
-  console.log("e", e);
-  count.value--;
-};
+const randomAge = () => {
+  // person.value = {
+  //   ...person.value,
+  //   age: Math.round(Math.random() * 100),
+  // };
 
-//films
-const movies = ref([
-  "Servante Ecarlate",
-  "Black List",
-  "Expanse",
-  "Matrix",
-  "Titanic",
-]);
-
-const deleteMovie = (movie) => {
-  movies.value = movies.value.filter((m) => m != movie);
-};
-
-//trier les films
-const sortMovies = () => {
-  movies.value.sort((a, b) => (a > b ? 1 : -1));
-};
-
-//add new moovie
-const movieName = ref("");
-const addMovie = () => {
-  // e.preventDefault()
-  movies.value.push(movieName.value);
-  movieName.value = "";
+  person.value.age = Math.round(Math.random() * 100);
 };
 </script>
-
-<style>
-/* h1 {
-  color: red;
-} */
-</style>
