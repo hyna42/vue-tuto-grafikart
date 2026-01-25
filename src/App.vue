4c6255d6adc3b30406cea6a9d83a6e1aa3d280a2
hyna42 <bylo@duck.com>
2026-01-25 21:19:50 +0100

    feat: watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,26 @@
-<!-- TODOLIST -->
+<!-- LES WATCHERS -->
 
 <template>
-  <form action="" @submit.prevent="addNewtask">
-    <fieldset role="group">
-      <input type="text" placeholder="Tâche à effectuer" v-model="taskTitle" />
-      <button :disabled="!taskTitle.length">Ajouter</button>
-    </fieldset>
-  </form>
-
-  <p v-if="!taskList.length">Vous n'avz pas de tâches à faire :(</p>
-
-  <div v-else>
-    <ul>
-      <li
-        v-for="task in sortedTasks"
-        :key="task.date"
-        :class="{ completed: task.completed }"
-      >
-        <Checkbox
-          :label="task.title"
-          @check="(p) => console.log('coché', p)"
-          @uncheck="console.log('décoché')"
-          v-model="task.completed"
-        />
-      </li>
-    </ul>
-
-    <Checkbox :label="'Masquer les tâches réalisées'" v-model="hideCompleted" />
-    <p v-if="remainingTodos">
-      {{ remainingTodos }} tâche{{ remainingTodos > 1 ? "s" : "" }} à faire
-    </p>
-  </div>
+  <input type="text" v-model="page.title" />
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from "vue";
-import Checkbox from "./Checkbox.vue";
-import Button from "./Button.vue";
+import { ref, watch, watchEffect } from "vue";
 
-//liste de tâches
-const taskList = ref([]);
-
-//tâche dynamique
-const taskTitle = ref("");
-
-//creer une nouvelle tâche
-const addNewtask = () => {
-  taskList.value = [
-    ...taskList.value,
-    {
-      title: taskTitle.value,
-      completed: false,
-      date: Date.now(),
-    },
-  ];
-  taskTitle.value = "";
-};
-//récupérer la liste des taches depuis une API au montage
-onMounted(() => {
-  fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((r) => r.json())
-    .then(
-      (v) => (taskList.value = v.map((todo) => ({ ...todo, date: todo.id }))),
-    );
+const page = ref({
+  title:''
 });
 
-console.log("datas :::",taskList)
-//filtrer tâches
-const sortedTasks = computed(() => {
-  console.log("demo");
-  const sortedTodos = taskList.value.toSorted((a, b) =>
-    a.completed > b.completed ? 1 : -1,
-  );
-
-  if (hideCompleted.value === true) {
-    return sortedTodos.filter((t) => t.completed === false);
-  }
-  return sortedTodos;
-});
-
-//nombre de taches a faire
-const remainingTodos = computed(() => {
-  return taskList.value.filter((t) => t.completed === false).length;
-});
-
-//variable pour masquer les tâches cochées
-const hideCompleted = ref(false);
+// watch(
+//   name,
+//   (newValue, oldvalue) => {
+//     document.title = newValue;
+//   },
+//   { immediate: true },
+// );
+watchEffect(() => {
+  document.title = page.value.title
+})
 </script>
 
-<style>
-li {
-  list-style: none;
-}
-
-.completed {
-  opacity: 0.5;
-  text-decoration: line-through;
-  /* text-decoration: strike-through; */
-}
-</style>
+<style></style>
