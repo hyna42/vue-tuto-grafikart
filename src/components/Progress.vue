edd374f22bddf40ce3f4b49f01a67998fb0f3887
hyna42 <bylo@duck.com>
2026-01-26 22:49:33 +0100

    feat: add Progress component to display step progress with dynamic values

---
+++ src/components/Progress.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            Etape : {{ value +1 }} / {{ max +1 }}
+        </h1>
+        <progress :max="max" :value="value"/>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    value: Number,
+    max:Number
+})
+</script>
