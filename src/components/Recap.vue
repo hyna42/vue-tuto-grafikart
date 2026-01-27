5d45d500a26bc51445c7d1161816b6717e4822cc
hyna42 <bylo@duck.com>
2026-01-27 20:06:08 +0100

    feat: enhance Recap component with score calculation and win condition logic

--- src/components/Recap.vue
+++ src/components/Recap.vue
@@ -1,6 +1,25 @@
 <template>
-    <h1>Recap</h1>
+  <h1>Recap</h1>
+  <p>{{ hasWon ? quiz.success_message : quiz.failure_message }}</p>
+  <p>Score : {{ score }} / {{ quiz.questions.length }}</p>
 </template>
 
 <script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  quiz: Object,
+  answers: Array,
+});
+
+const score = computed(() => {
+  return props.quiz.questions.reduce((acc, quetion, k) => {
+    if (quetion.correct_answer === props.answers[k]) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+});
+
+const hasWon = computed(() => score.value >= props.quiz.minimum_score);
 </script>
